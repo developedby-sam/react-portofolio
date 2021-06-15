4ed77037b1690af49aa155680cd131b6b0b498a9
Samir Ansari <developedbysam@gmail.com>
2021-06-15 21:52:51 +0545

    added FAQs-section without styles

---
+++ src/components/faq/FAQ.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+
+export default function FAQs() {
+	return (
+		<div className="faq">
+			<h2>
+				Any Questions <span>FAQ</span>
+			</h2>
+			<div className="question">
+				<h4>How Do I Start ?</h4>
+				<div className="answer">
+					<p>Lorem ipsum dolor sit amet.</p>
+					<p>
+						Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam,
+						debitis.
+					</p>
+				</div>
+			</div>
+
+			<div className="question">
+				<h4>Daily Schedules ?</h4>
+				<div className="answer">
+					<p>Lorem ipsum dolor sit amet.</p>
+					<p>
+						Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam,
+						debitis.
+					</p>
+				</div>
+			</div>
+
+			<div className="question">
+				<h4>Different Payments Methods ?</h4>
+				<div className="answer">
+					<p>Lorem ipsum dolor sit amet.</p>
+					<p>
+						Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam,
+						debitis.
+					</p>
+				</div>
+			</div>
+
+			<div className="question">
+				<h4>What Products Do You Offer ?</h4>
+				<div className="answer">
+					<p>Lorem ipsum dolor sit amet.</p>
+					<p>
+						Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam,
+						debitis.
+					</p>
+				</div>
+			</div>
+		</div>
+	);
+}
--- src/pages/AboutUs/AboutUs.jsx
+++ src/pages/AboutUs/AboutUs.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import About from "../../components/about/About.jsx";
 import Services from "../../components/services/Services.jsx";
+import FAQs from "../../components/faq/FAQ.jsx";
 
 export default function AboutUs() {
 	return (
 		<>
 			<About />
 			<Services />
+			<FAQs />
 		</>
 	);
 }

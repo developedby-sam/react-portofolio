0dd92172723d8acc51a38c8d6336cc2d2f1144cd
Samir Ansari <developedbysam@gmail.com>
2021-06-15 23:21:36 +0545

    added styles for the about-section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import GlobalStyle from "./GlobalStyles.js";
 
 import AboutUs from "./pages/AboutUs/AboutUs.jsx";
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <AboutUs />
     </div>
   );
--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,32 +1,66 @@
 import React from "react";
+import styled from "styled-components";
 import Home1 from "../../img/home1.png";
+
+const AboutContainer = styled.div`
+	min-height: 90vh;
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	padding: 5rem 10rem;
+	color: white;
+`;
+
+const Description = styled.div`
+	flex: 1;
+	padding-right: 5rem;
+
+	h2 {
+		font-weight: lighter;
+	}
+`;
+
+const Image = styled.div`
+	flex: 1;
+	overflow: hidden;
+
+	img {
+		width: 100%;
+		height: 80vh;
+		object-fit: cover;
+	}
+`;
+
+const Hide = styled.div`
+	overflow: hidden;
+`;
 
 export default function About() {
 	return (
-		<div>
-			<div className="description">
+		<AboutContainer>
+			<Description>
 				<div className="title">
-					<div className="hide">
+					<Hide>
 						<h2>We work to make</h2>
-					</div>
-					<div className="hide">
+					</Hide>
+					<Hide>
 						<h2>
 							your <span>dreams</span> come
 						</h2>
-					</div>
-					<div className="hide">
+					</Hide>
+					<Hide>
 						<h2>true</h2>
-					</div>
+					</Hide>
 				</div>
 				<p>
 					Contact us for any photography or videography ideas that you have. We
 					have professionals with amazing skills.
 				</p>
 				<button>Contact Us</button>
-			</div>
-			<div className="image">
-				<img src={Home1} alt="Guy with the camera" />>
-			</div>
-		</div>
+			</Description>
+			<Image>
+				<img src={Home1} alt="Guy with the camera" />
+			</Image>
+		</AboutContainer>
 	);
 }

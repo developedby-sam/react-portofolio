973c19dfad2e02ea0587500e78432635401fd190
Samir Ansari <developedbysam@gmail.com>
2021-06-17 22:47:04 +0545

    added nav component

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import React from "react";
 import GlobalStyle from "./GlobalStyles.js";
 
 import AboutUs from "./pages/AboutUs/AboutUs.jsx";
+import Nav from "./components/nav/Nav.js";
+
 function App() {
   return (
     <div className="App">
+      <Nav />
       <GlobalStyle />
       <AboutUs />
     </div>
